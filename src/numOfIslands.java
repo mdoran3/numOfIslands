9f4fc4a7251f831db20fdfdd83d8efad4da81ee8
mdoran3 <mdoran3@asu.edu>
2024-02-20 18:25:15 -0600

    Add annotations to code

--- src/numOfIslands.java
+++ src/numOfIslands.java
@@ -2,10 +2,10 @@ public class NumOfIslands {
 
     public int numIslands(char[][] grid) {
         int count = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
+        for (int i = 0; i < grid.length; i++) {             //loop through the rows of the grid
+            for (int j = 0; j < grid[i].length; j++) {      //loop through the columns of the grid
+                if (grid[i][j] == '1') {                    //if the current cell is 1, increment count and call bfs
+                    count++;                               
                     bfs(grid, i, j);
                 }
             }
@@ -13,8 +13,9 @@ public class NumOfIslands {
         return count;
     }
 
+    //Breadth First Search
     private void bfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') { //check if the current cell is out of bounds or is 0
             return;
         } else {
             grid[i][j] = '0';
